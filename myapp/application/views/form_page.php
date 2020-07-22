b9708050c428fa4b270e24898ac054c6c393cb60
Julien <julienbroens@gmail.com>
2020-07-22 16:29:39 +0200

    add Form.php & form_page

---
+++ myapp/application/views/form_page.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Welcome to CodeIgniter</title>
+
+</head>
+
+<body>
+
+
+    <h1>Form page</h1>
+
+
+
+
+</body>
+
+</html>
