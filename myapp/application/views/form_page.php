41ae8cbb43b3e817d4b5a644af0f1e24bea91596
Julien <julienbroens@gmail.com>
2020-07-23 11:09:08 +0200

    clean code

--- myapp/application/views/form_page.php
+++ myapp/application/views/form_page.php
@@ -131,31 +131,6 @@
         }
     }
 
-
-
-    // if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    //     echo $email . '<br>';
-    // } else {
-    //     var_dump("email Invalide");
-    // }
-
-
-    // if (isset($_POST['message'])) {
-
-    //     $entete  = 'MIME-Version: 1.0' . "\r\n";
-    //     $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-    //     $entete .= 'From: ' . $_POST['email'] . "\r\n";
-
-    //     $message = '<h1>Message envoyé depuis la page Contact de monsite.fr</h1>
-    //     <p><b>Nom : </b>' . $_POST['nom'] . '<br>
-    //     <b>Email : </b>' . $_POST['email'] . '<br>
-    //     <b>Message : </b>' . $_POST['message'] . '</p>';
-
-    //     $retour = mail('destinataire@free.fr', 'Envoi depuis page Contact', $message, $entete);
-    //     if ($retour) {
-    //         echo '<p>Votre message a bien été envoyé.</p>';
-    //     }
-    // }
     ?>
 
 
